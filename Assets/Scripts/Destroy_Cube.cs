9ddff8916bace5d46b5fcecd2c89dd9ca41c9936
morita-r <morita.cstsukuba@gmail.com>
2017-01-09 12:58:16 +0900

    create Destroy

---
+++ Assets/Scripts/Destroy_Cube.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy_Cube : MonoBehaviour {
+
+    public GameObject Cube;
+    public GameObject Fragment;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonUp(0))
+            Clicked();
+	}
+
+    private void Clicked()
+    {
+        Vector3 clicked_pos = Input.mousePosition;
+        clicked_pos.z = 10;
+        if (Cube.transform.position.x - 0.5f < Camera.main.ScreenToWorldPoint(clicked_pos).x
+            && Cube.transform.position.x + 0.5f > Camera.main.ScreenToWorldPoint(clicked_pos).x
+            && Cube.transform.position.y - 0.5f < Camera.main.ScreenToWorldPoint(clicked_pos).y
+            && Cube.transform.position.y + 0.5f > Camera.main.ScreenToWorldPoint(clicked_pos).y)
+            Cube_Clicked();
+    }
+
+    private void Cube_Clicked() {
+        Destroy(Cube);
+        GameObject frag_temp = Instantiate(Fragment,new Vector3(),Quaternion.identity);
+        frag_temp.GetComponent<Rigidbody>().AddForce(new Vector3(), ForceMode.VelocityChange);
+    }
+}
